85eaeb121b5ef0987f2daf03b124da8640d35364
Gaurav <gauravgahlyan27@gmail.com>
2020-03-07 11:29:37 +0530

    css

--- demo_app/urls.py
+++ demo_app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views.home,name='home'),
-    path('data',views.name_data,name='data')
-]
+    path('data',views.name_data,name='data'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    
+    ]
--- demo_app/views.py
+++ demo_app/views.py
@@ -1,19 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse,HttpResponseRedirect
 from .models import detail
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 from .forms import detail_form
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='/login/')
 def home(request):
     form = detail_form()
     if request.method == 'POST':
         form = detail_form(request.POST)
         if form.is_valid():
             form.save()
-        return render(request,'home.html',{'f':form})
+            return render(request,'home.html',{'f':form})
     else:
         return render(request,'home.html',{'f':form})
 
+@login_required(login_url='/login/')
 def name_data(request):
     data = (detail.objects.all())
     
     return render(request,'detail.html',{'data':data})
+
+def register(request):
+    if request.method == 'POST':
+        f = UserCreationForm(request.POST)
+        if f.is_valid():
+            f.save()
+            # messages.success(request, 'Account created successfully')
+            return redirect('/login')
+    else:
+        f = UserCreationForm()
+ 
+    return render(request, 'register.html', {'form': f})
