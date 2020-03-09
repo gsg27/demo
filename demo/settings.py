0fbf0c189ed83151970d0e3567aecf86839183fa
gsg27 <44452774+gsg27@users.noreply.github.com>
2020-03-09 23:17:08 +0530

    Update settings.py

--- demo/settings.py
+++ demo/settings.py
@@ -124,3 +124,7 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
